30833417ef9dc8a2b9461662445b445e858c581c
Viacheslav <1.slava.1.amelin.1@gmail.com>
2022-12-14 23:45:21 +0200

    feat: added orm and env

--- ff-backend/src/app.module.ts
+++ ff-backend/src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 @Module({
-  imports: [],
   controllers: [AppController],
   providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: Number(process.env.DB_PORT) || 3306,
+      username: process.env.DB_USERNAME || '',
+      password: process.env.DB_PASSWORD || '',
+      database: process.env.DB_DATABASE || '',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
